4d5d377b08ee3e8c5815ab95f7f36fce04d387d6
SLEEPlESS <rosas_u@ymail.com>
2022-05-24 21:58:22 -0700

    made typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,5 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
+// module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,38 @@ const { gql } = require('apollo-server-express');
 
 const  typeDefs = gql `
 type User {
-    _id:
+    _id: ID
+    username: String
+    friends: [User]
+    events: [Event]
 }
 
+type Event {
+    _id: ID
+    name: String
+    description: String
+    date: String
+    location: String
+    guests: [User]
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
 type Query {
-    
+    me: User
+    getEvent (_id: ID!): Event
+    getEvents (username: String): [Event]
 }
 
 Type Mutation {
-
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addEvent(name: String!, description: String!, date: String!, location: String!, guests: [User]): Event
+    addGuest(username: String!): Event 
+    addFriends (friendId: ID!): User
 }
 `;
 
