5cc0fb3146f061f7bc9a33f6086bfbd8548afdd5
HBK <sammcowen@gmail.com>
2022-05-24 21:16:21 -0700

    created front end server, renders hello world frompages/ home.js. proxy error( econnrefused )

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Auth from '../utils/auth';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello World!!!</h1>
+        </div>
+    );
+};
+
+export default Home;
