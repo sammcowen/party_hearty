2702bc1dcd7342673ccb6600d718e798be5c74e6
HBK <sammcowen@gmail.com>
2022-05-26 22:10:36 -0700

    temp landing page created with temp top nav.login and signup link do not work as of current

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,4 +2,25 @@
 // home button 
 // profile button
 // if modal for login/signUp logOut need button
-// add event 
+// add event
+import React from 'react';
+import url from '../../pages/Login';
+import url1 from '../../pages/Signup';
+
+function Nav () {
+    return (
+            <div>
+                <nav>
+                    
+                        <a href={url}>Log In</a>
+                        <a href={url1}>Sign Up</a>
+                        
+                   
+                </nav>
+            </div>
+    );
+};
+
+
+export default Nav;
+
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Jumbotron from '../components/Jumbotron';
+import Nav from '../components/Nav';
 // import Auth from '../utils/auth';
 // import { useQuery } from '@apollo/client';
 // import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <div>
             <Jumbotron></Jumbotron>
+            <Nav></Nav>
             <div className='text'>
             <h2>Plan your next event with us ...</h2>
             </div>
