1710370d7e0969d5cd6c661f6819e081964900a5
HBK <sammcowen@gmail.com>
2022-05-28 11:33:23 -0700

    created form component and entered it into eventformpage.js

---
+++ client/src/pages/EventFormPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron';
+import Nav from '../components/Nav';
+import Carousel from '../components/Carousel';
+import EventForm from '../components/EventForm';
+const EventFormPage = () => {
+    return (
+        <div>
+            <Carousel></Carousel>
+            <Nav></Nav>
+            <h2>
+                Fill out the form below to create your next Party Hearty Event!
+            </h2>
+            <EventForm></EventForm>
+            </div>
+      
+    );
+};
+
+export default EventFormPage;
+
