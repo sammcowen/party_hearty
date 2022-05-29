b12a6707bf9fd1b3ab4c9c087282d1a0297697e9
HBK <sammcowen@gmail.com>
2022-05-28 20:16:56 -0700

    updated client/src/components/EventForm/index.js by invoking the mutation in the component

--- client/src/components/EventForm/index.js
+++ client/src/components/EventForm/index.js
@@ -3,60 +3,66 @@ import { useMutation } from '@apollo/client';
 import { ADD_EVENT } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
-function EventForm () {
-    const [eventName,  seteventName] = React.useState('');
-    const [eventDescription, seteventDescription]  = React.useState('');
-    const[eventFee,seteventFee]  = React.useState('');
-    const[eventDate, seteventDate]  = React.useState('');
-    const[eventLocation,seteventLocation]  = React.useState('');
-    const[eventGuests,seteventGuests]  = React.useState('');
+function EventForm(props) {
+    // const[formState, setFormState] = useState({ eventName:'',eventDescription:'', eventDate:'', eventLocation:'', eventFee:''});
+    const [addEvent] = useMutation(ADD_EVENT);
+    const [eventName, seteventName] = React.useState('');
+    const [eventDescription, seteventDescription] = React.useState('');
+    const [eventFee, seteventFee] = React.useState('');
+    const [eventDate, seteventDate] = React.useState('');
+    const [eventLocation, seteventLocation] = React.useState('');
+    // const [eventGuests, seteventGuests] = React.useState('');
 
-    function handleSubmit(event) {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         console.log(eventName);
-        console.log('eventDescription', eventDescription);
-        console.log('eventFee',eventFee);
-        console.log('eventDate',eventDate);
-        console.log('eventLocation',eventLocation);
-        console.log('eventGuests',eventGuests);
         
 
-    }
-    return (
-        <div className='formstyle'>
-        <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="eventName" >Event Name :</label>
-                    <input type="text" id="eventName" value ={eventName} onChange={(e) => seteventName(e.target.value)} placeholder="Samm's Birthday"/>
-                </div>
-                <div >
-                    <label htmlFor="eventDescription" >Event Description :</label>
-                    <textarea  id="eventDescription" value ={eventDescription} onChange={(e) => seteventDescription(e.target.value)} rows="2"></textarea>
-                </div>
-                <div>
-                    <label htmlFor="eventFee" >Event Fee :</label>
-                    <input type="text" id="eventFee" value ={eventFee} onChange={(e) => seteventFee(e.target.value)}placeholder="150"/>
-                </div>
-                <div>
-                    <label htmlFor="eventDate" >Event Date :</label>
-                    <input type="text" id="eventDate" value ={eventDate} onChange={(e) => seteventDate(e.target.value)}placeholder="June 16"/>
-                </div>
-                <div>
-                    <label htmlFor="eventLocation" >Event Location :</label>
-                    <input type="text" id="eventLocation" value ={eventLocation} onChange={(e) => seteventLocation(e.target.value)}placeholder="My House"/>
-                </div>
-                <div>
-                    <label htmlFor="eventGuests" >Event Date :</label>
-                    <input type="text" id="eventGuests" value ={eventGuests}
-                    onChange={(e) => seteventGuests(e.target.value)} placeholder="Everyone!"/>
-                </div>
-                <button type="submit">Submit Your Event</button>
-            </form>
-            
+        const mutationRes = await addEvent({
+            variables: {
+                eventName: seteventName,
+                eventDescription: seteventDescription,
+                eventDate: seteventDate,
+                eventLocation: seteventLocation,
+                eventFee: seteventFee,
+            }
+        })
+        const token = mutationRes.data.addEvent.token;
+        Auth.login(token);
+    };
+        return (
+            <div className='formstyle'>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="eventName" >Event Name :</label>
+                        <input type="text" id="eventName" value={eventName} onChange={(e) => seteventName(e.target.value)} placeholder="Samm's Birthday" />
+                    </div>
+                    <div >
+                        <label htmlFor="eventDescription" >Event Description :</label>
+                        <textarea id="eventDescription" value={eventDescription} onChange={(e) => seteventDescription(e.target.value)} rows="2"></textarea>
+                    </div>
+                    <div>
+                        <label htmlFor="eventDate" >Event Date :</label>
+                        <input type="text" id="eventDate" value={eventDate} onChange={(e) => seteventDate(e.target.value)} placeholder="June 16" />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="eventLocation" >Event Location :</label>
+                        <input type="text" id="eventLocation" value={eventLocation} onChange={(e) => seteventLocation(e.target.value)} placeholder="My House" />
+                    </div>
+                    <div>
+                        <label htmlFor="eventFee" >Event Fee :</label>
+                        <input type="text" id="eventFee" value={eventFee} onChange={(e) => seteventDate(e.target.value)} placeholder="June 16" />
+                    </div>
+                    <button type="submit">Submit Your Event</button>
+                </form>
+
             </div>
-    );
     
+        );
 
-};
 
-export default EventForm;
+    };
+
+
+    export default EventForm;
