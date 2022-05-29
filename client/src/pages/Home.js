06bb4dc25c0dad0950f508294cd757d337adfd72
HBK <sammcowen@gmail.com>
2022-05-29 15:15:57 -0700

    changged icons to images and fixing the cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 // import Jumbotron from '../components/Jumbotron';
 import Nav from '../components/Nav';
+import Plan from '../assets/plan.jpg';
+import Fee from '../assets/fee.jpg';
+import Invite from '../assets/invite.jpg';
+
 
 import Carousel from '../components/Carousel';
 // import Auth from '../utils/auth';
@@ -13,31 +17,59 @@ const Home = () => {
             <div className='text'>
                 <h1 className="text">Party Hearty</h1>
             </div>
-            <Carousel/>
-            <Nav/>
+            <Carousel />
+            <Nav />
             <div className='text'>
                 <h2>Plan your next event with us ...</h2>
-                </div>
+            </div>
             <div className='homestep'>
-                <div className="card col-9">
-                    <div className="card-body">
-                    <i className="fa-solid fa-list-check"></i>   Plan your next birthday, graduation or any CELEBRATION!
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src={Plan} class="img-fluid rounded-start" alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+
+                                <p class="card-text">Plan you next birthday, graduation or any CELEBRATION with us!</p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="card col-9">
-                    <div className="card-body">
-                    <i className="fa-solid fa-comment-dollar"></i>  Create a guestlist , add a location, even add a fee for your event!
+                <div class="card mb-3" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src={Fee} class="img-fluid rounded-start" alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+
+                                <p class="card-text">Create a guestlist , add a location .. even add a FEE for your event!</p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="card col-9">
-                    <div className="card-body">
-                    <i className="fa-solid fa-rss"></i>  Post your event publicly or privately.. and keep track of your RSVPS !
+                <div class="card mb-3" >
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src={Invite} class="img-fluid rounded-start" alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+
+                                <p class="card-text">Post your event publicly or privately.. and keep track of your RSVPS !!</p>
+
+                            </div>
+                        </div>
                     </div>
-                </div>
                 </div>
 
             </div>
-       
+        </div>
+
+
 
     );
 };
