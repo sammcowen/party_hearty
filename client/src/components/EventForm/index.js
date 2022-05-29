a0c8c99a845237719232f31b8e8bb77f2acd0846
HBK <sammcowen@gmail.com>
2022-05-29 11:58:33 -0700

    trying to fix form to capture data and send to back end

--- client/src/components/EventForm/index.js
+++ client/src/components/EventForm/index.js
@@ -1,53 +1,53 @@
-import React from 'react';
+import React , { useState }from 'react';
 // import { useMutation } from '@apollo/client';
 // import { ADD_EVENT } from '../../utils/mutations';
 // import Auth from '../../utils/auth';
 
 function EventForm(props) {
-    // const[formState, setFormState] = useState({ eventName:'',eventDescription:'', eventDate:'', eventLocation:'', eventFee:''});
+    const[formState, setFormState] = useState({ eventName:'',eventDescription:'', eventDate:'', eventLocation:'', eventFee:''});
     // const [addEvent] = useMutation(ADD_EVENT);
-    const [eventName, seteventName] = React.useState('');
-    const [eventDescription, seteventDescription] = React.useState('');
-    const [eventFee, seteventFee] = React.useState('');
-    const [eventDate, seteventDate] = React.useState('');
-    const [eventLocation, seteventLocation] = React.useState('');
+    const {eventName, eventDescription, eventDate, eventLocation, eventFee} = formState;
+    // const [eventName, seteventName] = React.useState('');
+    // const [eventDescription, seteventDescription] = React.useState('');
+    // const [eventFee, seteventFee] = React.useState('');
+    // const [eventDate, seteventDate] = React.useState('');
+    // const [eventLocation, seteventLocation] = React.useState('');
     // const [eventGuests, seteventGuests] = React.useState('');
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(eventName);
-        console.log(eventDate);
-        console.log(eventLocation);
-        console.log(eventFee);
-        console.log(eventDescription);
+    function handleChange(e) {
+       setFormState({...formState, [e.target.name]: e.target.value})
         
 
        
     };
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    };
     
         return (
-            <div className='formstyle'>
+            <div id="eventform"className='formstyle'>
                 <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="eventName" >Event Name :</label>
-                        <input type="text" id="eventName" value={eventName} onChange={(e) => seteventName(e.target.value)} placeholder="Samm's Birthday" />
+                        <input type="text" name="eventName" defaultValue={eventName}  onChange={handleChange} placeholder="Samm's Birthday" />
                     </div>
                     <div >
                         <label htmlFor="eventDescription" >Event Description :</label>
-                        <textarea id="eventDescription" value={eventDescription} onChange={(e) => seteventDescription(e.target.value)} rows="2"></textarea>
+                        <textarea name="eventDescription" defaultValue={eventDescription} onChange={handleChange} rows="2"></textarea>
                     </div>
                     <div>
                         <label htmlFor="eventDate" >Event Date :</label>
-                        <input type="text" id="eventDate" value={eventDate} onChange={(e) => seteventDate(e.target.value)} placeholder="June 16" />
+                        <input type="text" name="eventDate" defaultValue={eventDate} onChange={handleChange} placeholder="June 16" />
                     </div>
                     
                     <div>
                         <label htmlFor="eventLocation" >Event Location :</label>
-                        <input type="text" id="eventLocation" value={eventLocation} onChange={(e) => seteventLocation(e.target.value)} placeholder="My House" />
+                        <input type="text" name="eventLocation" defaultValue={eventLocation} onChange={handleChange} placeholder="My House" />
                     </div>
                     <div>
                         <label htmlFor="eventFee" >Event Fee :</label>
-                        <input type="text" id="eventFee" value={eventFee} onChange={(e) => seteventFee(e.target.value)} placeholder="June 16" />
+                        <input type="text" name="eventFee" defaultValue={eventFee} onChange={handleChange} placeholder="June 16" />
                     </div>
                     <button type="submit">Submit Your Event</button>
                 </form>
