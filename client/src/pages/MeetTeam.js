19eac8634c53952f0f38b9393024d9a0f4ae9b33
HBK <sammcowen@gmail.com>
2022-05-30 23:06:27 -0700

    removed carousel from the meet teamoage

--- client/src/pages/MeetTeam.js
+++ client/src/pages/MeetTeam.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
-import Carousel from '../components/Carousel';
+// import Carousel from '../components/Carousel';
 import MeetTeamComp from "../components/MeetTeam";
 
 function MeetTeam () {
@@ -12,7 +12,7 @@ function MeetTeam () {
                 <div className='text'>
                     <h1 className="text">Party Hearty</h1>
                 </div>
-                <Carousel/>
+               
                 <Nav/>
             </div>
             <MeetTeamComp/>
