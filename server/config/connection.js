db28bb4b39df9bc89bb9193751c4da8ad52c5a7a
Buubonic <nguyenb.jim@gmail.com>
2023-06-14 18:53:06 -0700

    fix local

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/newDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/newDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
