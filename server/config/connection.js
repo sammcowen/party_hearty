756a62cda96cc7f7df7e3235cb51529a268d1333
Buubonic <nguyenb.jim@gmail.com>
2023-06-20 18:35:31 -0700

    change port

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/newDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/newDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
